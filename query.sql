bbeb57873995a97d04461eac3e4c55eaf96bbc9b
jan463 <jangfeller@gmail.com>
2025-02-18 21:25:03 +0100

    updates

--- query.sql
+++ query.sql
@@ -1,7 +1,38 @@
 USE final_project;
 
 -- 1
--- find 5 dishes with most calories per serving
-SELECT name, calories FROM sql_df
+-- find 3 dishes with most calories per serving
+SELECT name, calories 
+FROM sql_df
 ORDER BY calories DESC
-LIMIT 10;
+LIMIT 3;
+
+-- 2
+-- number of top rated recipes from japan
+SELECT COUNT(DISTINCT(name))
+FROM sql_df
+WHERE nations LIKE "%japanese%"
+ORDER BY aggregatedrating DESC;
+
+-- 3
+-- average calorie count from american desserts
+SELECT AVG(calories) AS average_calories
+FROM sql_df
+WHERE recipecategory LIKE "%dessert%"
+AND nations LIKE "%american%";
+
+-- 4
+-- find examples of greek soup
+SELECT name
+FROM sql_df
+WHERE recipecategory LIKE "%soup%"
+AND nations LIKE "%greek%";
+
+-- 5
+-- find examples of irish snacks
+SELECT name
+FROM sql_df
+WHERE recipecategory LIKE "%snack%"
+AND nations LIKE "%hawaiian%";
+
+
